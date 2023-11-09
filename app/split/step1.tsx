31af0b6bcec9fef6d7f75e2c8002e7163f89781d
Luca Georges Francois <luca@quartz.technology>
2023-11-09 18:48:38 +0100

    feat(split): add empty step placeholders
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

---
+++ app/split/step1.tsx
@@ -0,0 +1,10 @@
+import {CardBody} from "@nextui-org/react";
+import React from "react";
+
+export default function Step1() {
+  return (
+    <CardBody>
+      <p>This is the step 1.</p>
+    </CardBody>
+  );
+}

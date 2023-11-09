9b2da22a0ef94a4a9faa5000aa887a5f7c9a3110
Luca Georges Francois <luca@quartz.technology>
2023-11-09 18:47:23 +0100

    feat(components): update navigation bar
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,10 +7,7 @@ import {
 	NavbarItem,
 	NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
 
 import { link as linkStyles } from "@nextui-org/theme";
 
@@ -22,9 +19,6 @@ import { ThemeSwitch } from "@/components/theme-switch";
 import {
 	TwitterIcon,
 	GithubIcon,
-	DiscordIcon,
-	HeartFilledIcon,
-	SearchIcon,
 } from "@/components/icons";
 
 import { Logo } from "@/components/icons";

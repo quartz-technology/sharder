49a27f9953308c05a5c9f72cdd23e3729af1bd40
Luca Georges Francois <luca@quartz.technology>
2023-11-09 18:49:02 +0100

    feat(split): update main page
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- app/split/layout.tsx
+++ app/split/layout.tsx
@@ -6,8 +6,8 @@ export default function SplitLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
+		<section className="flex flex-col h-full items-center justify-center gap-4 py-8 md:py-10">
+			<div className="inline-block w-full max-w-2xl h-full text-center justify-center">
 				{children}
 			</div>
 		</section>

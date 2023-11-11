820ae18f055752d3c599411c7435a56219347d50
cleopha <coline@seguret.net>
2023-11-11 14:56:00 +0100

    add(combine): drop zone with File list implementation
    Signed-off-by: cleopha <coline@seguret.net>

--- app/combine/page.tsx
+++ app/combine/page.tsx
@@ -1,22 +1,73 @@
 "use client";
 
-import {subtitle, title} from "@/components/primitives";
-import {
-	Card,
-	CardBody,
-	Dropdown,
-	DropdownItem, DropdownMenu, DropdownTrigger
-} from "@nextui-org/react";
-import {Button} from "@nextui-org/button";
 import React from "react";
+import { Card, CardBody, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
+import { Button } from "@nextui-org/button";
+import { DropZone } from '@react-spectrum/dropzone';
+import Upload from '@spectrum-icons/illustrations/Upload';
+import { Content, IllustratedMessage } from '@adobe/react-spectrum';
+import { useDrag } from '@react-aria/dnd';
+import { FileTrigger, Heading, Text } from "react-aria-components";
+import { subtitle, title } from "@/components/primitives";
 
+/**
+ * Draggable component allows elements to be draggable.
+ * It uses useDrag hook from @react-aria/dnd for drag functionalities.
+ */
+function Draggable() {
+	const { dragProps, isDragging } = useDrag({
+		getItems() {
+			return [{
+				'text/plain': 'hello world',
+				'my-app-custom-type': JSON.stringify({ message: 'hello world' })
+			}];
+		}
+	});
+
+	return (
+		<div
+			{...dragProps}
+			role="button"
+			tabIndex={0}
+			className={`draggable ${isDragging ? 'dragging' : ''}`}
+		>
+		</div>
+	);
+}
+
+/**
+ * CombinePage component provides a user interface for file combining functionality.
+ */
 export default function CombinePage() {
 	const [selectedFileFormat, setSelectedFileFormat] = React.useState(new Set([".txt"]));
+	const [isFilled, setIsFilled] = React.useState(false);
+	const [files, setFiles] = React.useState<File[]>([]);
+
 
 	const selectedValue = React.useMemo(
 		() => Array.from(selectedFileFormat).join(", ").replaceAll("_", " "),
 		[selectedFileFormat]
 	);
+
+	const handleDrop = (event: any) => {
+		let droppedFiles: File[] = [];
+		event.items.forEach(async (item: any) => {
+			if (item.kind === "file") {
+				const file = await item.getFile();
+				droppedFiles.push(file);
+			}
+		});
+		setFiles([...files, ...droppedFiles]);
+		setIsFilled(true);
+	};
+
+	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.files) {
+			const selectedFiles = Array.from(event.target.files);
+			setFiles([...files, ...selectedFiles]);
+			setIsFilled(true);
+		}
+	};
 
 	return (
 		<div className={"flex flex-col w-full h-full justify-between"}>
@@ -25,7 +76,28 @@ export default function CombinePage() {
 			<div className={"flex w-full h-full justify-between"}>
 				<Card className={"w-full mt-4 mb-4"}>
 					<CardBody>
-						There should be a dropzone here for the shares.
+						<Draggable />
+						<DropZone
+							flex
+							isFilled={isFilled}
+							onDrop={handleDrop}>
+							<IllustratedMessage>
+								<Upload />
+								<Heading className={"mt-4"}>
+									<Text slot="label">
+										{isFilled ? 'You dropped something!' : 'Drag and drop here'}
+									</Text>
+								</Heading>
+								<Content>
+									<FileTrigger
+										onSelect={()=> setIsFilled(true)}>
+										<Button variant={"bordered"} radius={"none"} onClick={() => document.getElementById("combine-zone")?.click()}>Browse</Button>
+										<input style={{ display: "none" }} id={"combine-zone"} type="file" onChange={handleFileChange}/>
+									</FileTrigger>
+								</Content>
+							</IllustratedMessage>
+						</DropZone>
+
 					</CardBody>
 				</Card>
 			</div>
